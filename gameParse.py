c3d71b075672d3692eaeb7ba73c5e7222dc4133c
Corey <cpatri2@pdx.edu>
2023-02-15 11:58:49 -0800

    Added new method that makes new API request to get data about specified app ID from steam then parses to find the final formatted price of the game

--- gameParse.py
+++ gameParse.py
@@ -3,9 +3,9 @@ import json
 
 
 def main():
-    game = "PUBG: BATTLEGROUNDS"
+    game = "Monster Sanctuary"
     gameID = getAppID(game)
-    print(gameID)
+    getAppPrice(gameID)
 
 
 def getAppID(appName):
@@ -18,6 +18,19 @@ def getAppID(appName):
     for app in appDict["apps"]:
         if app["name"] == appName:
             return app["appid"]
+
+
+def getAppPrice(appID):
+    response = requests.get(
+        "http://store.steampowered.com/api/appdetails?appids=" + str(appID) + "&cc=us&l=en")
+    # jprint(response.json())
+    appIDString = str(appID)
+    app = response.json()
+    appDetails = app[str(appIDString)]
+    appData = appDetails["data"]
+    priceDetails = appData["price_overview"]
+    appPrice = priceDetails["final_formatted"]
+    print(type(appPrice))
 
 
 if __name__ == '__main__':
