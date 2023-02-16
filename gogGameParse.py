da3f5ef227332377dbf1e80498d93c2d7e14730b
Corey <cpatri2@pdx.edu>
2023-02-16 12:22:32 -0800

    removed some unnecessary commented out code

--- gogGameParse.py
+++ gogGameParse.py
@@ -5,7 +5,6 @@ import json
 def returnGamePrice(game):
     response = requests.get(
         "https://embed.gog.com/games/ajax/filtered?search=" + game)
-    # jprint(response.json())
     gameJson = response.json()
     gameList = gameJson["products"]
     for item in gameList:
@@ -16,8 +15,3 @@ def returnGamePrice(game):
     else:
         notFound = "Item not found"
         reuturn = notFound
-
-
-# def jprint(obj):
-#     text = json.dumps(obj, sort_keys=True, indent=4)
-#     print(text)
