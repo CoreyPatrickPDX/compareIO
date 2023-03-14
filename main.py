02d24ca729f58e2364a3c58b78073bd6ef9a6a1e
Corey <cpatri2@pdx.edu>
2023-03-14 08:52:41 -0700

    Created simple GUI to take a game as input and show price of game on Steam and GOG

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
 import webbrowser
+import PySimpleGUI as sg
 import APIConnections.gogGameParse as gog
 import APIConnections.steamGameParse as steam
 
 
 def main():
-    game = input("Please enter the game you wish to buy:").lower()
+    textTitle = sg.Text('Please enter game:', key="-OUT-",
+                        font=('Arial Bold', 16), expand_x=True, justification='left')
+    inputText = sg.Input('', enable_events=True, key='-INPUT-',
+                         font=('Arial Bold', 16), expand_x=True, justification='left')
+    searchButton = sg.Button('Search', key='-SEARCH-', font=('Arial Bold', 16))
+    exitButton = sg.Button('Exit', font=('Arial Bold', 16))
+    textGamePrice = sg.Text('Game Prices', key="-OUT-",
+                            font=('Arial Bold', 20), expand_x=True, justification='center')
+    steamText = sg.Text('Price on Steam:', key="-OUT-",
+                        font=('Arial Bold', 16), justification='left')
+    steamPriceText = sg.Text('', key="-STEAM PRICE-",
+                             font=('Arial Bold', 16), justification='left')
+    gogText = sg.Text('Price on GOG:', key="-OUT-",
+                      font=('Arial Bold', 16), justification='left')
+    gogPriceText = sg.Text('', key="-GOG PRICE-",
+                           font=('Arial Bold', 16), justification='left')
+
+    layout = [[textTitle, inputText],
+              [searchButton],
+              [textGamePrice],
+              [steamText, steamPriceText],
+              [gogText, gogPriceText],
+              [exitButton]]
+    window = sg.Window("compareIO", layout)
+
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == '-SEARCH-':
+            game = values['-INPUT-']
+
+            steamPrice, steamPriceFormatted, steamUrl = steam.returnPrice(
+                game.lower())
+            gogPrice, gogPriceFormatted, gogUrl = gog.returnGamePrice(
+                game.lower())
+            window['-STEAM PRICE-'].update(steamPriceFormatted)
+            window['-GOG PRICE-'].update(gogPriceFormatted)
+        if event == 'Exit' or event == sg.WIN_CLOSED:
+            break
+    window.close()
+
+    """ game = input("Please enter the game you wish to buy:").lower()
     steamPrice, steamPriceFormatted, steamUrl = steam.returnPrice(game)
     gogPrice, gogPriceFormatted, gogUrl = gog.returnGamePrice(game)
 
@@ -35,7 +77,7 @@ def main():
             break
 
     if (openStore.lower() == 'y'):
-        webbrowser.open(bestPriceUrl, new=1)
+        webbrowser.open(bestPriceUrl, new=1) """
 
 
 if __name__ == '__main__':
